0bc68cf8cc2e094be3364040b91a5be030b427f2
Diurno <Diurno@DAM115>
2024-12-11 12:49:51 +0100

    11/12

---
+++ Calculadora/src/CalculadoraGuillermo/CalculadoraGuillermo.java
@@ -0,0 +1,5 @@
+package CalculadoraGuillermo;
+
+public class CalculadoraGuillermo {
+
+}
---
+++ Practica_Arrays/src/practica2/Ejercicio3_SustituirValores.java
@@ -0,0 +1,42 @@
+package practica2;
+
+import java.util.Scanner;
+
+public class Ejercicio3_SustituirValores {
+	public static void main(String[] args) {
+
+		Scanner entrada = new Scanner(System.in);
+
+		int numeros[] = new int[20];
+
+		int valorAnter = 0;
+		int valorNuevo = 0;
+
+		for (int i = 0; i < numeros.length; i++) {
+
+			numeros[i] = (int) (0 + Math.random() * (20 - 0 + 1));
+
+			System.out.println("Numero " + i + ": " + numeros[i]);
+
+		}
+		
+		System.out.println("Valor que quieres cambiar : ");
+		valorAnter = entrada.nextInt();
+		System.out.println("Valor Nuevo :");
+		valorNuevo = entrada.nextInt();
+		
+		for (int i = 0; i < numeros.length; i++) {
+
+			if(numeros[i] == valorAnter) 
+			{
+				
+				numeros[i] = valorNuevo;
+				
+			}
+
+			System.out.println("Numero " + i + ": " + numeros[i]);
+			
+		}
+	}
+
+}
