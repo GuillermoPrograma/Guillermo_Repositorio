8afce06f6288780f17bca19fad5111aabc661fd3
Diurno <Diurno@DAM115>
2024-12-12 08:37:16 +0100

    12

--- Programacion_G/src/Ud5_Ejercicios_2/Ejercicio3_SumaMatriz.java
+++ Programacion_G/src/Ud5_Ejercicios_2/Ejercicio3_SumaMatriz.java
@@ -5,6 +5,11 @@ import java.util.Scanner;
 public class Ejercicio3_SumaMatriz {
 public static void main(String[] args) {
 	Scanner entrada = new Scanner (System.in);
+	System.out.println("Dimensiones");
+	int dim = entrada.nextInt();
+	
+	int m[] = new int[dim];
+	int n[] = new int[dim];
 	
 }
 }
