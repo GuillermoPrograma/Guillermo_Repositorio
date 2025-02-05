3c1565c5e47d047de212bffcf5dad1dfa2686568
Diurno <Diurno@DAM115>
2025-02-05 13:40:21 +0100

    05

--- programacion_Guille/src/ProgramacionHerencias/GestionamosCuentas.java
+++ programacion_Guille/src/ProgramacionHerencias/GestionamosCuentas.java
@@ -10,7 +10,24 @@ public class GestionamosCuentas {
 		titular1.setEdad(23);
 		titular1.setSaldo(2000);
 		
+		Titular titular2 = new Titular();
+		titular2.setNombre("Milka");
+		titular2.setApellidos("FFGG");
+		titular2.setEdad(26);
+		titular2.setSaldo(100);
 		
+		CuentaCorriente cuenta1 = new CuentaCorriente(titular1,"ES203984",titular1.getSaldo());
+		CuentaAhorro cuenta2 = new CuentaAhorro(titular2,"ES298784",titular2.getSaldo());
+		
+		cuenta1.Ingresar(200);
+		cuenta1.Reintegro(2000);
+		cuenta1.Imprimir();
+		System.out.println("Cuenta2 :");
+		cuenta2.Imprimir();
+		cuenta2.CalcularInteres();
+		cuenta2.Imprimir();
+		System.out.println(cuenta2.getInteres());
+	
 		
 	}
 }
