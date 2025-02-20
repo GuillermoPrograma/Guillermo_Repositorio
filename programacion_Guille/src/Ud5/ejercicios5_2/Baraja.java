b53b057717db39d0a22d42719ab8576a441b01e6
Diurno <Diurno@DAM115>
2025-02-20 12:35:33 +0100

    20

--- programacion_Guille/src/Ud5/ejercicios5_2/Baraja.java
+++ programacion_Guille/src/Ud5/ejercicios5_2/Baraja.java
@@ -3,28 +3,55 @@ package Ud5.ejercicios5_2;
 public class Baraja {
 
 	private Carta baraja[] = new Carta[40];
+	private int numCartaActual = 0;
+	private Carta dadas[];
 
 	public Baraja(Carta[] baraja) {
 		this.baraja = baraja;
 	}
 
+	public void crearBaraja() 
+	{
+		
+		
+	}
+	
 	public void barajar() {
 		int aleatorio;
-		int contador = 0;
+
 		Carta baraja2[] = new Carta[40];
 		for (int i = 0; i < baraja.length; i++) {
-			
+
 			aleatorio = (int) (Math.random() * (40 + 1));
 			while (baraja2[aleatorio] != null)
 				aleatorio = (int) (Math.random() * (40 + 1));
-			if (baraja2[aleatorio] == null) {
-				
-				baraja[0] = baraja2[aleatorio];
-			}
-		
+
+			baraja[i] = baraja2[aleatorio];
+		}
+		this.baraja = baraja2;
+		numCartaActual = 0;
+	}
+
+	public void siguiente_carta() {
+		if (numCartaActual <= 40) {
+			System.out.println(baraja[numCartaActual].toString());
+			baraja[numCartaActual] = null;
+			numCartaActual = +1;
+		} else
+			System.out.println("No quedan cartas disponibles");
+	}
+
+	public void cartasDisponibles() {
+		System.out.println("Quedan disponibles : " + (baraja.length - numCartaActual) + " Cartas.");
+	}
+
+	public void MostrarBaraja() {
+
+		for (int i = numCartaActual; i < baraja.length; i++) {
+
+			System.out.println(baraja[i]);
 
 		}
 
 	}
-
 }
