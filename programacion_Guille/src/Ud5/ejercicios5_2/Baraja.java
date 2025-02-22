4d7d4fa847ef5188663b46da55fbd418928b98c9
guill <guill@Guillermo>
2025-02-22 20:10:46 +0100

    22

--- programacion_Guille/src/Ud5/ejercicios5_2/Baraja.java
+++ programacion_Guille/src/Ud5/ejercicios5_2/Baraja.java
@@ -2,10 +2,11 @@ package Ud5.ejercicios5_2;
 
 public class Baraja {
 
-	
 	private int numCartaActual = 0;
 	private Carta dadas[] = new Carta[40];
 	private int descartadas;
+	private Carta monton[] = new Carta[40];
+	private int contadorMonton = 0;
 
 	public Baraja() {
 
@@ -18,29 +19,29 @@ public class Baraja {
 			for (int j = 0; j <= 9; j++) {
 				if (i == 0) {
 					if (j > 6) {
-						dadas[j] = new Carta(j + 3, Palos.Bastos);
+						dadas[Carta.getNumCarta()] = new Carta(j + 3, Palos.Bastos);
 					} else
-						dadas[j] = new Carta(j + 1, Palos.Bastos);
+						dadas[Carta.getNumCarta()] = new Carta(j + 1, Palos.Bastos);
 				}
 				if (i == 1) {
 					if (j > 6) {
-						dadas[j] = new Carta(j + 3, Palos.Copas);
+						dadas[Carta.getNumCarta()] = new Carta(j + 3, Palos.Copas);
 					} else
-						dadas[j] = new Carta(j + 1, Palos.Copas);
+						dadas[Carta.getNumCarta()] = new Carta(j + 1, Palos.Copas);
 				}
 				if (i == 2) {
 					if (j > 6) {
-						dadas[j] = new Carta(j + 3, Palos.Espadas);
+						dadas[Carta.getNumCarta()] = new Carta(j + 3, Palos.Espadas);
 					} else
-						dadas[j] = new Carta(j + 1, Palos.Espadas);
+						dadas[Carta.getNumCarta()] = new Carta(j + 1, Palos.Espadas);
 				}
 				if (i == 3) {
 					if (j > 6) {
-						dadas[j] = new Carta(j + 3, Palos.Sotas);
+						dadas[Carta.getNumCarta()] = new Carta(j + 3, Palos.Sotas);
 					} else
-						dadas[j] = new Carta(j + 1, Palos.Sotas);
+						dadas[Carta.getNumCarta()] = new Carta(j + 1, Palos.Sotas);
 				}
-				
+
 			}
 
 		}
@@ -56,41 +57,76 @@ public class Baraja {
 			aleatorio = (int) (Math.random() * (39 + 1));
 			while (baraja2[aleatorio] != null)
 				aleatorio = (int) (Math.random() * (39 + 1));
-			
+
 			baraja2[aleatorio] = dadas[i];
-			
-		}//ss
+
+		} // ss
 		this.dadas = baraja2;
 		numCartaActual = 0;
 	}
 
 	public void siguiente_carta() {
-		if (numCartaActual <= 40) {
+		if (numCartaActual < 40) {
+			
 			System.out.println(dadas[numCartaActual].toString());
+			monton[contadorMonton] = dadas[numCartaActual];
 			dadas[numCartaActual] = null;
-			descartadas = +1;
-			numCartaActual = +1;
+			descartadas ++;
+			numCartaActual ++;
+			contadorMonton++;
 		} else
 			System.out.println("No quedan cartas disponibles");
 	}
 
-	public void cartasDisponibles() {
+	public int cartasDisponibles() {
 		int disponibles = 0;
-		
-		for(int i = 0; i < dadas.length; i++) 
-		{
-			if(dadas[i] != null)
-			disponibles += 1;
-		}
-		
-		System.out.println("Quedan disponibles : " + (disponibles) + " Cartas.");
-	}
-
-	public void MostrarBaraja() {
 
 		for (int i = 0; i < dadas.length; i++) {
-			if(dadas[i] != null)
-			System.out.println(dadas[i]);
+			if (dadas[i] != null)
+				disponibles += 1;
+		}
+		return disponibles;
+
+	}
+
+	public Carta[] darCartas(int num) {
+		Carta[] devueltas = new Carta[num];
+		int disponibles = cartasDisponibles();
+		int contador = 0;
+		if (disponibles > num) {
+			for (int i = numCartaActual; i < numCartaActual + num; i++) {
+				if (dadas[i] != null) {
+					devueltas[contador] =dadas[i];
+					monton[contadorMonton] =dadas[i];
+					dadas[i] = null;
+					numCartaActual ++;
+					descartadas ++;
+					contadorMonton++;}
+				
+				else
+					num++;
+			}
+			return devueltas;
+
+		} else {
+			System.out.println("No hay suficientes cartas para darte");
+		return null;}
+	}
+
+	public void mostrarMonton() {
+		for (int i = 0; i < monton.length; i++) {
+			
+			if(monton[i] != null)
+				System.out.println(monton[i].toString());
+		}
+
+	}
+
+	public void mostrarBaraja() {
+
+		for (int i = 0; i < dadas.length; i++) {
+			if (dadas[i] != null)
+				System.out.println(dadas[i]);
 
 		}
 
--- programacion_Guille/src/Ud5/ejercicios5_2/Prueba.java
+++ programacion_Guille/src/Ud5/ejercicios5_2/Prueba.java
@@ -4,11 +4,11 @@ public class Prueba {
 
 	 public static void main(String[] args) {
 		Baraja baraja1 = new Baraja();
-		baraja1.MostrarBaraja();
 		
 		
-		
-
+		baraja1.siguiente_carta();
+		baraja1.darCartas(10);
+		baraja1.mostrarMonton();
 	
 	}
 }
