30dda8a63792d69d9a265e890e4d75139d692b8b
Diurno <Diurno@DAM115>
2025-03-18 13:31:14 +0100

    18

--- programacion_Guille/src/Ud9/Excepciones/Ejercicio1_PruebaNext.java
+++ programacion_Guille/src/Ud9/Excepciones/Ejercicio1_PruebaNext.java
@@ -8,14 +8,12 @@ public class Ejercicio1_PruebaNext {
 	public static void main(String[] args) {
 
 		Scanner entrada = new Scanner(System.in);
-		
+
 		try {
 			int nums = entrada.nextInt();
-		}
-		catch(InputMismatchException e) 
-		{
+		} catch (InputMismatchException e) {
 			System.out.println("Solo puede ser valor ");
-			
+
 		}
 	}
 
