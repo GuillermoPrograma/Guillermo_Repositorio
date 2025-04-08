a395ab40e67da214ae1656a25faaeb93c217a1d9
guill <guill@DESKTOP-FTINI27>
2025-04-08 19:58:27 +0200

    08/04

---
+++ programacion_Guille/src/Ud10/colecciones/ColeccionDeDatos/SegundaTanda/Ejercicio2_Listas.java
@@ -0,0 +1,21 @@
+package Ud10.colecciones.ColeccionDeDatos.SegundaTanda;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Ejercicio2_Listas {
+
+	List<String> lista = new ArrayList<>();
+	
+	
+	
+	void agregarNombresEstudiantes(String nombre) 
+	{
+		lista.add(nombre);
+	}
+	void insertarPosicionEspecifica(int posicion, String nombre) 
+	{
+		
+		
+	}
+}
